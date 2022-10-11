633ea578b4da2897d1bc11d37fc56ab02f3a0899
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-10-11 23:44:07 +0300

    minor update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,19 @@
 import { RouteRecordRaw } from 'vue-router'
 
 import MainLayout from 'layouts/MainLayout.vue'
+import IndexPage from 'pages/IndexPage.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: MainLayout,
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        name: 'index',
+        component: IndexPage,
+      },
+    ],
   },
 
   // Always leave this as last one,
