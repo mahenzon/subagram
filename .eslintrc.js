87a90dd457623f47fd9702e644e8d5c6e57632a6
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-10-11 22:44:18 +0300

    update packages, configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,24 @@
 module.exports = {
   // https://eslint.org/docs/user-guide/configuring#configuration-cascading-and-hierarchy
   // This option interrupts the configuration hierarchy at this file
-  // Remove this if you have an higher level ESLint config file (it usually happens into a monorepos)
+  // Remove this if you have an higher
+  //  level ESLint config file (it usually happens into a monorepos)
   root: true,
 
   // https://eslint.vuejs.org/user-guide/#how-to-use-a-custom-parser
   // Must use parserOptions instead of "parser" to allow vue-eslint-parser to keep working
-  // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
+  // `parser: 'vue-eslint-parser'` is already included
+  //   with any 'plugin:vue/**' config and should be omitted
   parserOptions: {
     parser: require.resolve('@typescript-eslint/parser'),
-    extraFileExtensions: [ '.vue' ]
+    extraFileExtensions: ['.vue'],
   },
 
   env: {
     browser: true,
     es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true
+    'vue/setup-compiler-macros': true,
   },
 
   // Rules order is important, please avoid shuffling them
@@ -32,11 +34,13 @@ module.exports = {
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
     'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
-    'airbnb-base'
-    
+    '@vue/eslint-config-typescript',
+
+    'airbnb-base',
+
   ],
 
   plugins: [
@@ -45,8 +49,8 @@ module.exports = {
 
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
     // required to lint *.vue files
-    'vue'
-    
+    'vue',
+
   ],
 
   globals: {
@@ -59,45 +63,77 @@ module.exports = {
     __QUASAR_SSR_PWA__: 'readonly',
     process: 'readonly',
     Capacitor: 'readonly',
-    chrome: 'readonly'
+    chrome: 'readonly',
   },
 
-  // add your custom rules here
   rules: {
-    
-    'no-param-reassign': 'off',
-    'no-void': 'off',
-    'no-nested-ternary': 'off',
-    'max-classes-per-file': 'off',
 
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    
-    'prefer-promise-reject-errors': 'off',
-
-    quotes: ['warn', 'single', { avoidEscape: true }],
-
-    // this rule, if on, would require explicit return type on the `render` function
-    '@typescript-eslint/explicit-function-return-type': 'off',
-
-    // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
-    '@typescript-eslint/no-var-requires': 'off',
-
-    // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
-    // does not work with type definitions
-    'no-unused-vars': 'off',
+    // 'no-param-reassign': 'off',
+    // 'no-void': 'off',
+    // 'no-nested-ternary': 'off',
+    // 'max-classes-per-file': 'off',
+    //
+    // 'no-shadow': 'off',
+    // '@typescript-eslint/no-shadow': 'error',
+    //
+    // 'import/first': 'off',
+    // 'import/named': 'error',
+    // 'import/namespace': 'error',
+    // 'import/default': 'error',
+    // 'import/export': 'error',
+    // 'import/extensions': 'off',
+    // 'import/no-unresolved': 'off',
+    // 'import/no-extraneous-dependencies': 'off',
+    // 'import/prefer-default-export': 'off',
+    //
+    // 'prefer-promise-reject-errors': 'off',
+    //
+    // quotes: ['error', 'single', { avoidEscape: true }],
+    //
+    // // this rule, if on, would require explicit return type on the `render` function
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    //
+    // // in plain CommonJS modules, you can't use `import foo = require('foo')`
+    // //  to pass this rule, so it has to be disabled
+    // '@typescript-eslint/no-var-requires': 'off',
+    //
+    // // The core 'no-unused-vars' rules (in the eslint:recommended ruleset)
+    // // does not work with type definitions
+    // 'no-unused-vars': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    // author-preffered
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'array-element-newline': [
+      'error',
+      {
+        ArrayExpression: 'consistent',
+        ArrayPattern: { minItems: 3 },
+      },
+    ],
+    // vue
+    'vue/block-lang': [
+      'error',
+      {
+        script: {
+          lang: 'ts',
+        },
+      },
+    ],
+    'vue/block-tag-newline': ['error'],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
+      },
+    ],
+  },
 }
