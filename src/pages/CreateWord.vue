5929e2852d838efcd25eb4fbd8b4b800af2d68cc
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-10-16 19:43:39 +0300

    add input for word creation

--- src/pages/CreateWord.vue
+++ src/pages/CreateWord.vue
@@ -1,9 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const word = ref<string>('')
 </script>
 
 <template>
-  <q-page-container>
-    <h1>Hello</h1>
-  </q-page-container>
+  <q-page padding>
+    <h3>{{ $t('wordCreate.title') }}</h3>
+    <q-banner class="bg-light-blue">
+      <h6 class="q-ma-sm">
+        <q-icon
+          name="info"
+          size="lg"
+        />
+        {{ $t('wordCreate.description') }}
+      </h6>
+    </q-banner>
+    <div
+      class="q-gutter-md row"
+    >
+      <q-input
+        v-model="word"
+        color="secondary"
+        class="col-12 col-md-4"
+        :label="$t('wordCreate.newWordInputLabel')"
+      />
+    </div>
+  </q-page>
 </template>
