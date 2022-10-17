2a032f588bbd98ad8859115ce0bf41100310456c
Suren Khorenyan <surenkhorenyan@gmail.com>
2022-10-17 23:13:02 +0300

    create common components scroller and create

---
+++ src/components/common/AddWordFAB.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page-sticky
+    position="bottom-right"
+    :offset="[18, 18]"
+  >
+    <q-btn
+      fab
+      :to="{ name: 'create-word' }"
+      size="lg"
+      color="accent"
+      icon="create"
+    />
+  </q-page-sticky>
+</template>
---
+++ src/components/common/PageUpScroller.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page-scroller position="bottom">
+    <q-btn
+      fab
+      icon="keyboard_arrow_up"
+      color="secondary"
+    />
+  </q-page-scroller>
+</template>
