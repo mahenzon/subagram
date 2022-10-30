8485f436c8bec7a0dfb3cbf7e945a7fccf2d97db
mahenzon <surenkhorenyan@gmail.com>
2022-10-30 15:43:24 +0000

    Deploying to gh-pages from @ mahenzon/subagram@86531fc43c5aea129f245d2e6668dbc9a1106a97 🚀

---
+++ assets/AboutPage.36adf2cd.js
@@ -0,0 +1,3 @@
+import{_ as c,o as t,m as d,w as s,n as l,t as a,C as r,D as i,F as u,p as _,q as m,y as p,z as f}from"./index.68bfae38.js";const g={},b={class:"text-h3 q-mb-md"};function h(e,x){return t(),d(f,{padding:""},{default:s(()=>[l("div",b,a(e.$t("about.title")),1),(t(!0),r(u,null,i(e.$t("about.text").split(`
+
+`),(o,n)=>(t(),r("p",{key:n,class:"text-h5"},a(o),1))),128)),_(p,{size:"lg",color:"secondary",to:{name:"create-word"}},{default:s(()=>[m(a(e.$t("about.create")),1)]),_:1})]),_:1})}var k=c(g,[["render",h]]);export{k as default};
